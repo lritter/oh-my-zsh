636a850aab8a47ab2d0b253a6fb43fa6733ac27e
Lincoln Ritter <lincoln.ritter@gmail.com>
2010-12-29 13:39:44 -0500

    Added clojure-contrib to classpath

--- environment.zsh
+++ environment.zsh
@@ -1,1 +1,5 @@
 export PATH="$PATH":"$HOME/local/bin"
+export EDITOR="mvim"
+export VISUAL="mvim"
+export PAGER="less"
+export CLASSPATH=$CLASSPATH:/usr/local/Cellar/clojure-contrib/1.2.0/clojure-contrib.jar
